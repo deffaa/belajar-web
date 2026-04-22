9c5fe397c8689edb3eecd7834b40e38ce9ce98d0
deffaa <haidarsdeffa@gmail.com>
2026-04-22 18:38:37 +0700

    deffa gey

--- z.js
+++ z.js
@@ -15,3 +15,30 @@ let nilai = [80, 90, 100];
 
 console.log("Hello");
 alert("Halo");
+
+function sapa(nama) {
+    return "Halo " + nama;
+}
+
+console.log(sapa("Deffa"));
+
+nilai = 80;
+
+if (nilai >= 75) {
+    console.log("Lulus");
+} else {
+    console.log("Tidak lulus");
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+const judul = document.querySelector("h1");    // Ambil tag
+const tombol = document.querySelector("#btn"); // Ambil ID
+const item = document.querySelector(".menu");  // Ambil Class
+
+judul.innerHTML = "<strong>Judul Baru</strong>"; // Render sebagai HTML
+judul.textContent = "Judul Baru";              // Render sebagai teks biasa
+judul.style.color = "red";           // Mengubah warna teks
+judul.style.backgroundColor = "blue"; // Menggunakan camelCase
+judul.style.display = "none";        // Menyembunyikan elemen
