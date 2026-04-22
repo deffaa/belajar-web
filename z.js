80a62200f30492d610eec711edc9a89195d2a002
deffaa <haidarsdeffa@gmail.com>
2026-04-22 18:06:11 +0700

    deffa gey

---
+++ z.js
@@ -0,0 +1,17 @@
+let nama = "Deffa";
+const umur = 19;
+let teks = "Hello";     // String
+let angka = 10;         // Number
+let aktif = true;       // Boolean
+let kosong = null;
+let belum;
+
+let mahasiswa = {
+    nama: "Deffa",
+    umur: 19
+};
+
+let nilai = [80, 90, 100];
+
+console.log("Hello");
+alert("Halo");
